76c57869a619e271db0a4f2ae16e5c29c5ec9253
fajar-apri-alaska <Fajar.Apriansyah@alaskaair.com>
2023-06-06 10:16:00 +0700

    chore: fix typo package name in postInstall script
    Changes to be committed:
    	modified:   packageScripts/postinstall.mjs

--- packageScripts/postinstall.mjs
+++ packageScripts/postinstall.mjs
@@ -21,7 +21,7 @@ console.log(chalk.hex('#f26135')(`
 ╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
 
         Thanks for installing the latest version
-                of `) + chalk.hex('#ffd200').bold(`auro-flightline v${pjson.version}.`) + chalk.hex('#f26135')(`
+                of `) + chalk.hex('#ffd200').bold(`auro-avatar v${pjson.version}.`) + chalk.hex('#f26135')(`
 
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
