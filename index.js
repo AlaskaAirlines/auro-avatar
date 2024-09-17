1de8c277356c184673a67a63dc228cba6098a846
Jordan Jones <jmjones2463@gmail.com>
2024-09-17 14:45:51 -0700

    refactor: update custom component registration config

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { AuroAvatar } from './src/auro-avatar.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
- const registerComponent = (name = 'custom-avatar') => {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroAvatar {});
-  }
-}
-
-export { registerComponent }
+RuntimeUtils.default.prototype.registerComponent('custom-avatar', AuroAvatar);
