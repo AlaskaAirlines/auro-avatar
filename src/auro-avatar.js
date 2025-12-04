945146f2adced6169a53836710b34eaaa612aa20
Chris Friedberg <chris.friedberg@alaskaair.com>
2025-12-04 14:58:57 -0800

    docs(deprecation): add deprecation notice to API documentation for tail attribute

--- src/auro-avatar.js
+++ src/auro-avatar.js
@@ -23,7 +23,7 @@ import tokensCss from "./styles/tokens.scss";
  * @attr {String} alt - provide string for element alt text
  * @attr {String} code - provide airport code for requested airport image
  * @attr {String} img - provide location or URL for image to be used
- * @attr {String} tail - provide tail logo for requested airline
+ * @attr {String} tail - **DEPRECATED** - provide tail logo for requested airline. Use the [auro-tail](https://github.com/AlaskaAirlines/auro-tail) component instead.
  * @deprecated The tail attribute is deprecated. Use the auro-tail component instead. See: https://github.com/AlaskaAirlines/auro-tail
  * @attr {String} type - modifiers for size of avatar (sm | md)
  */
